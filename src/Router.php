0a63fd2263717ef6f9f13551cce437459ff111c4
Eduardo <edustrd@gmail.com>
2023-03-13 20:38:04 -0300

    Just a test

--- src/Router.php
+++ src/Router.php
@@ -3,14 +3,36 @@
 namespace L03n\RouterPhp;
 
 // Router PHP
+// class Router {
+//     public $uri;
+
+//     public function __construct() {
+//         $this->uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+//     }
+
+//     public function getURI() {
+//         return $this->uri;
+//     }
+// }
+
+
 class Router {
     public $uri;
+
+    public $routes = [
+        '/' => 'hello routes',
+    ];
 
     public function __construct() {
         $this->uri = parse_url($_SERVER['REQUEST_URI'])['path'];
     }
 
-    public function getURI() {
-        return $this->uri;
+    public function getRoutes() {
+        if ($this->uri == '/') {
+            echo 'good uri; good boy<br><br>';
+            echo 'good start<br><br>' . $this->routes['/'];
+        } else {
+            echo 'come back to the base boy, missing some rocky';
+        }
     }
 }
